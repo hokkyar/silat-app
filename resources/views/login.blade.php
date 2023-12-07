54cbaf2e3e41a66fa9e5185c1465214f656f16de
hokkyar <hokkyaryasta@gmail.com>
2023-12-07 10:12:46 +0700

    title color updated on login screen

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,7 +12,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
-                    <h5 class="text-center mt-5">Sistem Informasi Manajemen Latihan</h5>
+                    <h5 class="text-center mt-5 text-dark">Sistem Informasi Manajemen Latihan</h5>
                     <div class="login-brand">
                         <img src="{{ asset('img/logo-koni.ico') }}" alt="logo" width="100" class="">
                     </div>
